1aae18d6a2a7c9d055f226e913d9fecc8df04503
kotyara12 <kotyara12@yandex.ru>
2023-05-17 16:44:02 +0300

    Implemented shooter control

--- include/reShutter.h
+++ include/reShutter.h
@@ -27,25 +27,33 @@ extern "C" {
 class rShutter;
 
 typedef bool (*cb_shutter_publish_t) (rShutter *shutter, char* topic, char* payload, bool free_topic, bool free_payload);
-typedef void (*cb_shutter_change_t) (rShutter *shutter, uint8_t step, uint8_t max_steps);
+typedef void (*cb_shutter_change_t) (rShutter *shutter, uint8_t from_step, uint8_t to_step, uint8_t max_steps);
 typedef bool (*cb_shutter_gpio_init_t) (rShutter *shutter, uint8_t pin, bool level_active);
 typedef bool (*cb_shutter_gpio_change_t) (rShutter *shutter, uint8_t pin, bool physical_level);
 
 class rShutter {
   public:
     rShutter(uint8_t pin_open, bool level_open, uint8_t pin_close, bool level_close, 
-      uint8_t max_steps, uint32_t step_time, float step_time_adj, uint32_t step_time_fin,
+      uint8_t max_steps, uint32_t full_time, uint32_t step_time, float step_time_adj, uint32_t step_time_fin,
       cb_shutter_change_t cb_state_changed, cb_shutter_publish_t cb_mqtt_publish);
     ~rShutter();
 
+    // Current state
     uint8_t getState();
     char* getJSON();
 
     bool Init();
+    // Open or close the shutter by a specified number of steps
     bool Open(uint8_t steps);
-    bool Close(uint8_t steps, bool forced);
+    bool Close(uint8_t steps);
+    // Forced close on initialization or crash
+    bool CloseForced();
 
-    void mqttSetCallback(cb_load_publish_t cb_publish);
+    // For the timer handler
+    bool StopAndQueueProcessing();
+
+    // MQTT
+    void mqttSetCallback(cb_shutter_publish_t cb_publish);
     char* mqttTopicGet();
     bool mqttTopicSet(char* topic);
     bool mqttTopicCreate(bool primary, bool local, const char* topic1, const char* topic2, const char* topic3);
@@ -60,18 +68,29 @@ class rShutter {
     virtual bool gpioInit() = 0;
     virtual bool gpioSetLevel(uint8_t pin, bool physical_level) = 0; 
   private:
-    uint8_t     _max_steps = 10;                  // Number of steps to fully open
-    uint32_t    _step_time = 1000;                // Time delay by one step in milliseconds
-    float       _step_time_adj = 1.00;            // Delay adjustment factor for each next step
-    uint32_t    _step_time_fin = 0;               // Finishing time
-    uint8_t     _state = 0;                       // Current state
-    time_t      _changed = 0;                     // Time of last state change
-    timer_t     _timer = nullptr;                 // Step timer
-    char*       _mqtt_topic = nullptr;            // MQTT topic
+    uint32_t            _full_time = 15000;       // Full closing time in milliseconds
+    uint8_t             _max_steps = 10;          // Number of steps to fully open
+    uint32_t            _step_time = 1000;        // Time delay by one step in milliseconds
+    float               _step_time_adj = 1.00;    // Delay adjustment factor for each next step
+    uint32_t            _step_time_fin = 0;       // Finishing time
+    uint8_t             _state = 0;               // Current state
+    uint8_t             _queue_open = 0;          // Number of scheduled steps if the timer is active
+    uint8_t             _queue_close = 0;         // Number of scheduled steps if the timer is active
+    time_t              _changed = 0;             // Time of last state change
+    esp_timer_handle_t  _timer = nullptr;         // Step timer
+    char*               _mqtt_topic = nullptr;    // MQTT topic
 
     cb_shutter_change_t  _on_changed = nullptr;   // Pointer to the callback function to be called after load switching
     cb_shutter_publish_t _mqtt_publish = nullptr; // Pointer to the publish callback function
 
+    uint32_t calcStepTimeout(uint8_t step);
+
+    // Disable all drives
+    bool StopAll();
+
+    // Timer
+    bool timerCreate();
+    bool timerFree();
     bool timerActivate(uint8_t pin, bool level, uint32_t duration_ms);
     bool timerIsActive();
     bool timerStop();
--- src/reShutter.cpp
+++ src/reShutter.cpp
@@ -17,13 +17,14 @@ static const char* logTAG = "SHTR";
 // -----------------------------------------------------------------------------------------------------------------------
 
 rShutter::rShutter(uint8_t pin_open, bool level_open, uint8_t pin_close, bool level_close, 
-  uint8_t max_steps, uint32_t step_time, float step_time_adj, uint32_t step_time_fin,
+  uint8_t max_steps, uint32_t full_time, uint32_t step_time, float step_time_adj, uint32_t step_time_fin,
   cb_shutter_change_t cb_state_changed, cb_shutter_publish_t cb_mqtt_publish)
 {
   _pin_open = pin_open;
   _level_open = level_open;
   _pin_close = pin_close;
   _level_close = level_close;
+  _full_time = full_time;
   _max_steps = max_steps;
   _step_time = step_time;
   _step_time_adj = step_time_adj;
@@ -35,11 +36,13 @@ rShutter::rShutter(uint8_t pin_open, bool level_open, uint8_t pin_close, bool le
   _changed = 0;
   _mqtt_topic = nullptr;
   _timer = nullptr;
+
+  timerCreate();
 }
 
 rShutter::~rShutter()
 {
-  timerStop();
+  timerFree();
   if (_mqtt_topic) free(_mqtt_topic);
   _mqtt_topic = nullptr;
 }
@@ -51,24 +54,129 @@ rShutter::~rShutter()
 bool rShutter::Init()
 {
   return gpioInit() 
-      && gpioSetLevel(_pin_open, !level_open) 
-      && gpioSetLevel(_pin_close, !level_close)
-      && Close(0, true);
+      && StopAll()
+      && CloseForced();
 }
 
+// Disable all drives
+bool rShutter::StopAll()
+{
+  return gpioSetLevel(_pin_open, !_level_open) && gpioSetLevel(_pin_close, !_level_close);
+}
+
+// At the end of the timer, we execute the next steps, if any
+bool rShutter::StopAndQueueProcessing()
+{
+  bool ret = StopAll();
+  if (ret) {
+    if (_queue_open > 0) {
+      ret = Open(_queue_open);
+    } else if (_queue_close > 0) {
+      ret = Close(_queue_close);
+    };
+    _queue_open = 0;
+    _queue_close = 0;
+  };
+  return ret;
+}
+
+
+// Current state
 uint8_t rShutter::getState()
 {
   return _state;
 }
 
-bool rShutter::Open(uint8_t steps)
+uint32_t rShutter::calcStepTimeout(uint8_t step)
 {
-
+  uint32_t ret = _step_time;
+  if (step > 1) {
+    for (uint8_t i = 2; i <= step; i++) {
+      ret = ret * _step_time_adj;
+    };
+  };
+  return ret;
 }
 
-bool rShutter::Close(uint8_t steps, bool forced)
+// Open the shutter by a specified number of steps
+bool rShutter::Open(uint8_t steps)
 {
-  
+  uint8_t _steps = steps;
+  if (_steps + _state > _max_steps) {
+    _steps = _max_steps - _state;
+    rlog_w(logTAG, "Requested %d steps, actually %d steps will be completed", steps, _steps);
+  };
+  if (_steps > 0) {
+    if (timerIsActive()) {
+      _queue_open += _steps;
+      rlog_w(logTAG, "An operation is currently in progress, the requested steps are queued");
+    } else {
+      // Calculate time
+      uint32_t _duration = 0;
+      for (uint8_t i = 1; i <= _steps; i++) {
+        _duration += calcStepTimeout(_state + _steps);
+      };
+      // Turn on the drive for the сalculated time
+      if (timerActivate(_pin_open, _level_open, _duration)) {
+        rlog_i(logTAG, "Open shutter %d steps ( %d milliseconds )", _steps, _duration);
+        if (_on_changed) {
+          _on_changed(this, _state, _state + _steps, _max_steps);
+        };
+        _state += _steps;
+        return true;
+      };
+      rlog_e(logTAG, "Failed to activate shutter");
+    };
+  };
+  return false;
+}
+
+// Close the shutter by a specified number of steps
+bool rShutter::Close(uint8_t steps)
+{
+  uint8_t _steps = steps;
+  if (_steps > _state) {
+    _steps = _state;
+    rlog_w(logTAG, "Requested %d steps, actually %d steps will be completed", steps, _steps);
+  };
+  if (_steps > 0) {
+    if (timerIsActive()) {
+      _queue_close += _steps;
+      rlog_w(logTAG, "An operation is currently in progress, the requested steps are queued");
+    } else {
+      // Calculate time
+      uint32_t _duration = 0;
+      for (uint8_t i = _steps; i > 0 ; i--) {
+        _duration += calcStepTimeout(_state - _steps + 1);
+      };
+      // Turn on the drive for the сalculated time
+      if (timerActivate(_pin_close, _level_close, _duration)) {
+        rlog_i(logTAG, "Close shutter %d steps ( %d milliseconds )", _steps, _duration);
+        if (_on_changed) {
+          _on_changed(this, _state, _state - _steps, _max_steps);
+        };
+        _state -= _steps;
+        return true;
+      };
+      rlog_e(logTAG, "Failed to activate shutter");
+    };
+  };
+  return false;
+}
+
+// Forced close on initialization or crash
+bool rShutter::CloseForced()
+{
+  timerStop();
+  if (timerActivate(_pin_close, _level_close, _full_time)) {
+    rlog_i(logTAG, "Сlose shutter completely");
+    if (_on_changed) {
+      _on_changed(this, _state, 0, _max_steps);
+    };
+    _state = 0;
+    return true;
+  };
+  return false;
 }
 
 // -----------------------------------------------------------------------------------------------------------------------
@@ -79,13 +187,11 @@ static void shutterTimerEnd(void* arg)
 {
   if (arg) {
     rShutter* shutter = (rShutter*)arg;
-    shutter->gpioSetLevel(_pin_open, !level_open);
-    shutter->gpioSetLevel(_pin_close, !level_close);
-    shutter->timerStop();
+    shutter->StopAndQueueProcessing();
   };
 }
 
-bool rShutter::timerActivate(uint8_t pin, bool level, uint32_t duration_ms)
+bool rShutter::timerCreate()
 {
   if (_timer == nullptr) {
     esp_timer_create_args_t cfg;
@@ -94,15 +200,30 @@ bool rShutter::timerActivate(uint8_t pin, bool level, uint32_t duration_ms)
     cfg.callback = shutterTimerEnd;
     cfg.arg = this;
     RE_OK_CHECK(esp_timer_create(&cfg, &_timer), return false);
-    if (_timer != nullptr) {
-      RE_OK_CHECK(esp_timer_start_once(_timer, duration_ms*1000), return false);
-      if (gpioSetLevel(pin, level)) {
-        return true;
-      } else {
-        esp_timer_stop(_timer);
-        esp_timer_delete(_timer);
-        _timer = nullptr;
-      };
+  };
+  return true;
+}
+
+bool rShutter::timerFree()
+{
+  if (_timer != nullptr) {
+    timerStop();
+    RE_OK_CHECK(esp_timer_delete(_timer), return false);
+  };
+  return true;
+}
+
+bool rShutter::timerActivate(uint8_t pin, bool level, uint32_t duration_ms)
+{
+  if (_timer == nullptr) {
+    timerCreate();
+  };
+  if (_timer != nullptr) {
+    RE_OK_CHECK(esp_timer_start_once(_timer, duration_ms*1000), return false);
+    if (gpioSetLevel(pin, level)) {
+      return true;
+    } else {
+      timerStop();
     };
   };
   return false;
@@ -117,10 +238,9 @@ bool rShutter::timerStop()
 {
   if (_timer != nullptr) {
     if (esp_timer_is_active(_timer)) {
-      esp_timer_stop(_timer);
+      StopAll();
+      RE_OK_CHECK(esp_timer_stop(_timer), return false);
     };
-    RE_OK_CHECK(esp_timer_delete(_timer), return false);
-    _timer = nullptr;
   };
   return true;
 }
@@ -129,7 +249,7 @@ bool rShutter::timerStop()
 // -------------------------------------------------------- MQTT ---------------------------------------------------------
 // -----------------------------------------------------------------------------------------------------------------------
 
-void rShutter::mqttSetCallback(cb_load_publish_t cb_publish)
+void rShutter::mqttSetCallback(cb_shutter_publish_t cb_publish)
 {
   _mqtt_publish = cb_publish;
 }
